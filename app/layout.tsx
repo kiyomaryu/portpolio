47c27b99192ded22cf3fd5bc40d465beb0051d32
きよまる <ki4labo@gmail.com>
2026-03-13 22:16:48 +0900

    OGP画像を設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,31 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Kiyomaru | Technical PdM / PMO / SRE",
+  title: "きよまる | Technical PdM / PMO",
   description:
-    "技術者出身のプロダクトマネージャー Kiyomaru のポートフォリオサイトです。",
+    "技術者出身のプロダクトマネージャー きよまる のポートフォリオサイトです。",
+  openGraph: {
+    title: "きよまる | Technical PdM / PMO",
+    description:
+      "技術者出身のプロダクトマネージャー きよまる のポートフォリオサイトです。",
+    url: "https://kiyomaruworks.com",
+    siteName: "きよまる Portfolio",
+    images: [
+      {
+        url: "https://kiyomaruworks.com/og-image.jpg",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "きよまる | Technical PdM / PMO",
+    description:
+      "技術者出身のプロダクトマネージャー きよまる のポートフォリオサイトです。",
+    images: ["https://kiyomaruworks.com/og-image.jpg"],
+  },
 };
 
 export default function RootLayout({
